a6c0d11411ca5b6e084d953c77295e171d3a9b52
juzybits <96890594+juzybits@users.noreply.github.com>
2023-08-28 16:39:51 +0000

    [web] basic App.tsx with timezones-to-times. Remove Home.tsx.

--- web/src/js/App.tsx
+++ web/src/js/App.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from 'react';
 import '../css/App.less';
-import { Home } from './Home';
 
 export const App: React.FC = () =>
 {
-    return <>
-    <div id='layout'>
-        <Home />
-    </div>
-    </>;
+    const [timezones, _setTimezones] = useState<string[]>([
+        'America/Los_Angeles',
+        'Europe/London',
+        'Asia/Tokyo'
+    ]);
+    const [times, setTimes]= useState<string[]>();
+
+    useEffect(() => {
+        const now = new Date();
+        const timeStrings = timezones.map(tz => dateToString(now, tz));
+        setTimes(timeStrings);
+    }, [timezones]);
+
+    return <div id='layout'>
+        Times:
+        <pre>
+            {JSON.stringify(times, null, 2)}
+        </pre>
+    </div>;
+}
+
+// Display a `Date` as "HH:mm" in the specific timezone
+function dateToString(date: Date, tz: string): string {
+        const timeInTimeZone = new Intl.DateTimeFormat('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+            timeZone: tz,
+            hour12: false,
+        }).format(date);
+        return timeInTimeZone;
 }
