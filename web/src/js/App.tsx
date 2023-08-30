92c3a7cf4da7446f5c065183a062a1e82cfb5f3d
juzybits <96890594+juzybits@users.noreply.github.com>
2023-08-30 13:10:29 +0000

    [web] move date functions to lib/time.ts

--- web/src/js/App.tsx
+++ web/src/js/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+import { dateToTime, dateToDay, dateToHours, getHourDiff } from './lib/time';
 import '../css/App.less';
 
 /*
@@ -24,7 +25,9 @@ export const App: React.FC = () =>
     );
 }
 
-// Show local time in a vertical column for each timezone
+/**
+ * Full-screen panel with a column for each timezone
+ */
 const ColumnsPanel: React.FC<{
     timezones: string[];
 }> = ({
@@ -56,6 +59,9 @@ const ColumnsPanel: React.FC<{
     );
 }
 
+/**
+ * Colorful column showing the timezone and city info
+ */
 const Column: React.FC<{
     now: Date;
     tz: string;
@@ -64,8 +70,8 @@ const Column: React.FC<{
     tz,
 }) =>
 {
-    const time = dateToTimeStr(now, tz);
-    const day = dateToDayStr(now, tz);
+    const time = dateToTime(now, tz);
+    const day = dateToDay(now, tz);
     const diff = getHourDiff(now, tz);
     const hour = dateToHours(now, tz);
     const backgroundImage = gradients[hour];
@@ -83,31 +89,34 @@ const Column: React.FC<{
     );
 }
 
+/**
+ * Column backgrounds for each of the 24 hours of the day
+ */
 const gradients = [
-    'linear-gradient(rgb(3, 12, 27), rgb(8, 9, 35))',
-    'linear-gradient(rgb(3, 12, 27), rgb(8, 9, 35))',
-    'linear-gradient(rgb(3, 12, 27), rgb(3, 12, 27))',
-    'linear-gradient(rgb(4, 15, 34), rgb(3, 12, 27))',
-    'linear-gradient(rgb(8, 28, 52), rgb(4, 15, 34))',
-    'linear-gradient(rgb(33, 83, 102), rgb(8, 28, 52))',
-    'linear-gradient(rgb(57, 138, 151), rgb(33, 83, 102))',
-    'linear-gradient(rgb(89, 184, 188), rgb(57, 138, 151))',
-    'linear-gradient(rgb(146, 205, 188), rgb(89, 184, 188))',
-    'linear-gradient(rgb(203, 225, 188), rgb(146, 205, 188))',
-    'linear-gradient(rgb(241, 237, 179), rgb(203, 225, 188))',
-    'linear-gradient(rgb(245, 234, 154), rgb(241, 237, 179))',
-    'linear-gradient(rgb(250, 231, 128), rgb(245, 234, 154))',
-    'linear-gradient(rgb(254, 228, 103), rgb(250, 231, 128))',
-    'linear-gradient(rgb(250, 201, 97), rgb(254, 228, 103))',
-    'linear-gradient(rgb(246, 175, 90), rgb(250, 201, 97))',
-    'linear-gradient(rgb(241, 149, 84), rgb(246, 175, 90))',
-    'linear-gradient(rgb(202, 118, 94), rgb(241, 149, 84))',
-    'linear-gradient(rgb(152, 87, 110), rgb(202, 118, 94))',
-    'linear-gradient(rgb(101, 56, 126), rgb(152, 87, 110))',
-    'linear-gradient(rgb(69, 36, 108), rgb(101, 56, 126))',
-    'linear-gradient(rgb(43, 19, 79), rgb(69, 36, 108))',
-    'linear-gradient(rgb(16, 2, 51), rgb(43, 19, 79))',
-    'linear-gradient(rgb(12, 6, 43), rgb(16, 2, 51))',
+    'linear-gradient(rgb(3, 12, 27), rgb(8, 9, 35))', // 00
+    'linear-gradient(rgb(3, 12, 27), rgb(8, 9, 35))', // 01
+    'linear-gradient(rgb(3, 12, 27), rgb(3, 12, 27))', // 02
+    'linear-gradient(rgb(4, 15, 34), rgb(3, 12, 27))', // 03
+    'linear-gradient(rgb(8, 28, 52), rgb(4, 15, 34))', // 04
+    'linear-gradient(rgb(33, 83, 102), rgb(8, 28, 52))', // 05
+    'linear-gradient(rgb(57, 138, 151), rgb(33, 83, 102))', // 06
+    'linear-gradient(rgb(89, 184, 188), rgb(57, 138, 151))', // 07
+    'linear-gradient(rgb(146, 205, 188), rgb(89, 184, 188))', // 08
+    'linear-gradient(rgb(203, 225, 188), rgb(146, 205, 188))', // 09
+    'linear-gradient(rgb(241, 237, 179), rgb(203, 225, 188))', // 10
+    'linear-gradient(rgb(245, 234, 154), rgb(241, 237, 179))', // 11
+    'linear-gradient(rgb(250, 231, 128), rgb(245, 234, 154))', // 12
+    'linear-gradient(rgb(254, 228, 103), rgb(250, 231, 128))', // 13
+    'linear-gradient(rgb(250, 201, 97), rgb(254, 228, 103))', // 14
+    'linear-gradient(rgb(246, 175, 90), rgb(250, 201, 97))', // 15
+    'linear-gradient(rgb(241, 149, 84), rgb(246, 175, 90))', // 16
+    'linear-gradient(rgb(202, 118, 94), rgb(241, 149, 84))', // 17
+    'linear-gradient(rgb(152, 87, 110), rgb(202, 118, 94))', // 18
+    'linear-gradient(rgb(101, 56, 126), rgb(152, 87, 110))', // 19
+    'linear-gradient(rgb(69, 36, 108), rgb(101, 56, 126))', // 20
+    'linear-gradient(rgb(43, 19, 79), rgb(69, 36, 108))', // 21
+    'linear-gradient(rgb(16, 2, 51), rgb(43, 19, 79))', // 22
+    'linear-gradient(rgb(12, 6, 43), rgb(16, 2, 51))', // 23
 ];
 
 /*
@@ -130,58 +139,3 @@ const AllColumns: React.FC = () => {
     );
 }
 */
-
-// Convert a `Date` to ['HH', 'mm'] in the specific timezone
-function dateToTimeStr(date: Date, tz: string): string[] {
-    const timeStr = new Intl.DateTimeFormat('en-US', {
-        timeZone: tz,
-        hour: '2-digit',
-        minute: '2-digit',
-        hourCycle: 'h23',
-
-    }).format(date);
-    return timeStr.split(':');
-}
-
-// Convert a `Date` to 'Mon 28', 'Fri 1', etc. in the specific timezone
-function dateToDayStr(date: Date, tz: string): string {
-    const dayStr = new Intl.DateTimeFormat('en-GB', {
-        timeZone: tz,
-        weekday: 'short',
-        day: 'numeric',
-    }).format(date);
-    return dayStr;
-}
-
-// Get the hour as a number between 0 and 23
-function dateToHours(date: Date, tz: string): number {
-    const dateStr = new Intl.DateTimeFormat('en-US', {
-        timeZone: tz,
-        hour: '2-digit',
-        hourCycle: 'h23',
-    }).format(date);
-
-    return parseInt(dateStr);
-}
-
-// Get the hour difference vs local time as '-8', '+5.5', '0', etc.
-function getHourDiff(date: Date, tz: string): string {
-    // Hours and minutes in local time
-    const localTotalMinutes = date.getUTCHours() * 60 + date.getUTCMinutes() + date.getTimezoneOffset();
-
-    // Hours and minutes in target timezone
-    const tzDate = new Date(date.toLocaleString('en-US', { timeZone: tz }));
-    const tzTotalMinutes = tzDate.getUTCHours() * 60 + tzDate.getUTCMinutes() + tzDate.getTimezoneOffset();
-
-    // Compute the difference in minutes
-    let differenceInMinutes = tzTotalMinutes - localTotalMinutes;
-
-    // Adjust for day overlap
-    if (differenceInMinutes > 720) differenceInMinutes -= 1440; // 720 minutes = 12 hours, 1440 minutes = 24 hours
-    if (differenceInMinutes < -720) differenceInMinutes += 1440;
-
-    // Convert difference to hours with possible decimal for half-hours
-    const diff = differenceInMinutes / 60;
-
-    return (diff >= 0 ? '+' : '') + diff;
-}
---
+++ web/src/js/lib/time.ts
@@ -0,0 +1,66 @@
+/**
+ * Utility functions for converting and comparing time and date values across different time zones.
+ */
+
+/**
+ * Convert a `Date` to ['HH', 'mm'] in the specific timezone
+ */
+export function dateToTime(date: Date, tz: string): string[] {
+    const timeStr = new Intl.DateTimeFormat('en-US', {
+        timeZone: tz,
+        hour: '2-digit',
+        minute: '2-digit',
+        hourCycle: 'h23',
+
+    }).format(date);
+    return timeStr.split(':');
+}
+
+/**
+ * Convert a `Date` to 'Mon 28', 'Fri 1', etc. in the specific timezone
+ */
+export function dateToDay(date: Date, tz: string): string {
+    const dayStr = new Intl.DateTimeFormat('en-GB', {
+        timeZone: tz,
+        weekday: 'short',
+        day: 'numeric',
+    }).format(date);
+    return dayStr;
+}
+
+/**
+ * Get the hour as a number between 0 and 23
+ */
+export function dateToHours(date: Date, tz: string): number {
+    const dateStr = new Intl.DateTimeFormat('en-US', {
+        timeZone: tz,
+        hour: '2-digit',
+        hourCycle: 'h23',
+    }).format(date);
+
+    return parseInt(dateStr);
+}
+
+/**
+ * Get the hour difference vs local time as '-8', '+5.5', '0', etc.
+ */
+export function getHourDiff(date: Date, tz: string): string {
+    // Hours and minutes in local time
+    const localTotalMinutes = date.getUTCHours() * 60 + date.getUTCMinutes() + date.getTimezoneOffset();
+
+    // Hours and minutes in target timezone
+    const tzDate = new Date(date.toLocaleString('en-US', { timeZone: tz }));
+    const tzTotalMinutes = tzDate.getUTCHours() * 60 + tzDate.getUTCMinutes() + tzDate.getTimezoneOffset();
+
+    // Compute the difference in minutes
+    let differenceInMinutes = tzTotalMinutes - localTotalMinutes;
+
+    // Adjust for day overlap
+    if (differenceInMinutes > 720) differenceInMinutes -= 1440; // 720 minutes = 12 hours, 1440 minutes = 24 hours
+    if (differenceInMinutes < -720) differenceInMinutes += 1440;
+
+    // Convert difference to hours with possible decimal for half-hours
+    const diff = differenceInMinutes / 60;
+
+    return (diff >= 0 ? '+' : '') + diff;
+}
