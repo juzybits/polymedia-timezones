29974795fc9a2b819ac0cb47f39b12d43d109fa4
juzybits <96890594+juzybits@users.noreply.github.com>
2023-09-02 03:46:42 +0000

    [web] add 'x' to modal

--- web/src/js/Modal.tsx
+++ web/src/js/Modal.tsx
@@ -15,6 +15,7 @@ export const Modal: React.FC<{
     return (
         <div className='modal-background' onClick={onClose}>
             <div className='modal-content' onClick={e => e.stopPropagation()}>
+                <label className='modal-close' onClick={onClose}>✕</label>
                 {/* We stop propagation to prevent the modal from closing when its content is clicked */}
                 {content}
             </div>
