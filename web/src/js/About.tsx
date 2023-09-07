8492fad1547864f4f0b793114d704b20cb68f7f8
juzybits <96890594+juzybits@users.noreply.github.com>
2023-09-07 02:50:41 +0000

    [web] #about-menu: keyboard shorcuts info

--- web/src/js/About.tsx
+++ web/src/js/About.tsx
@@ -24,13 +24,23 @@ export const AboutMenu: React.FC<{
     return (
         <div id='about-menu'>
             <h2>About</h2>
-            <p>Polymedia Timezones is a free and open-source tool.</p>
-            <p>The code is available on <a href='https://github.com/juzybits/polymedia-timezones' target='_blank' rel='noopener'>GitHub</a>.</p>
-            <p>To learn more about my work, visit <a href='https://polymedia.app' target='_blank' rel='noopener'>polymedia.app</a></p>
-            <p>If you have any feedback, feel free to reach out:
+            <p>Polymedia Timezones is <a href='https://github.com/juzybits/polymedia-timezones' target='_blank' rel='noopener'>open-source</a></p>
+            <p>See my other work at <a href='https://polymedia.app' target='_blank' rel='noopener'>polymedia.app</a></p>
+            <p>Send your feedback/bugs/requests:
                 <br/><a href='https://twitter.com/juzybits' target='_blank' rel='noopener'>@juzybits</a>
                 <br/><a href='https://twitter.com/polymedia_app' target='_blank' rel='noopener'>@polymedia_app</a>
                 <br/><a href='https://discord.gg/3ZaE69Eq78' target='_blank' rel='noopener'>Polymedia Discord</a></p>
+
+            <br/>
+            <hr/>
+            <div id='about-keyboard'>
+                <h2>Keyboard shortcuts</h2>
+                <p><span>+</span>open city menu</p>
+                <p><span>Up/Down</span>navigate city menu</p>
+                <p><span>Enter</span>add city to dashboard</p>
+                <p><span>Escape</span>close city menu</p>
+            </div>
+
         </div>
     );
 }
