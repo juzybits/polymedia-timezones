7d35872ae6fd74b48cc818198d42d9ef25ad1481
juzybits <96890594+juzybits@users.noreply.github.com>
2025-01-31 19:46:55 -0100

    pwa manifest.json with vite-plugin-pwa

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [
-        react(),
-    ],
     build: {
         chunkSizeWarningLimit: 1000,
     },
@@ -17,4 +15,48 @@ export default defineConfig({
     resolve: {
         dedupe: ['react', 'react-dom']
     },
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: "Polymedia Timezones",
+                short_name: "Timezones",
+                description: "The easiest way to keep track of time around the world.",
+                start_url: "/",
+                scope: "/",
+                display: "fullscreen",
+                background_color: "pink",
+                theme_color: "black",
+                icons: [
+                    {
+                        src: "/img/logo_1024x1024.webp",
+                        type: "image/webp",
+                        sizes: "1024x1024",
+                        purpose: "any"
+                    },
+                    {
+                        src: "/img/logo_1024x1024.webp",
+                        type: "image/webp",
+                        sizes: "1024x1024",
+                        purpose: "maskable"
+                    }
+                ],
+                screenshots: [
+                    {
+                        src: "/img/open-graph.webp",
+                        type: "image/webp",
+                        sizes: "2400x1200",
+                        form_factor: "wide"
+                    },
+                    {
+                        src: "/img/screenshot_1170x2532.webp",
+                        type: "image/webp",
+                        sizes: "1170x2532",
+                        form_factor: "narrow"
+                    }
+                ]
+            }
+        })
+    ],
 });
